8d93cfb1569f56cbf0fe4cdcc364331e5c0dcba3
nhatsinh97 <55687877+nhatsinh97@users.noreply.github.com>
2022-03-17 11:11:30 +0700

    Update uv2.py

--- uv2.py
+++ uv2.py
@@ -7,17 +7,20 @@ import time
 import os
 time.sleep(5)
 rtsp = 'rtsp://admin:Admin123@192.168.32.203/cam/realmonitor?channel=1&subtype=00&authbasic=YWRtaW46QWRtaW4xMjM='
+camout = 'rtsp://admin:Admin123@192.168.32.204/cam/realmonitor?channel=1&subtype=00&authbasic=YWRtaW46QWRtaW4xMjM='
 url = 'http://172.16.128.41:8089/api/Farm/postbiohistory'
 apitimer = 'http://172.16.128.41:8089/api/Farm/getcountdownsecond'
 mac_address = "6c:1c:71:5c:9b:31"  # "6c:1c:71:5c:9b:31" #"6c:1c:71:5b:6d:19"
 timer = '0'
 status = False
+check = False
 # Khai bao chi tiet cac thuoc tinh cua Port noi tiep
 r = requests.post(apitimer, data=json.dumps(
     {"mac_address": mac_address}), headers={'Content-type': 'application/json', 'Accept': 'text/plain'})
 data = r.json()
 if data > 0:
     status = True
+    check = True
     time.sleep(1)
 ser = serial.Serial(port='/dev/ttyS0', baudrate=115200, parity=serial.PARITY_NONE,
                     stopbits=serial.STOPBITS_ONE, bytesize=serial.EIGHTBITS, timeout=1)
@@ -65,6 +68,24 @@ try:
             if file == 200:
                 status = False
                 time.sleep(1)
+        if not check and (data == "checkout"):
+            time.sleep(1)
+            cap = cv2.VideoCapture(camout)
+            retval, img = cap.read()
+            strImg64 = base64.b64encode(
+                cv2.imencode('.jpg', img)[1]).decode()
+            r = requests.post(url, data=json.dumps({
+                "mac_address": mac_address,
+                "action_name": 'RECEIVE',
+                "timer": '',
+                "img": strImg64
+            }), headers={'Content-type': 'application/json',
+                            'Accept': 'text/plain'})
+            file = r.json()
+            if file == 200:
+                check = False
+                time.sleep(1)
+            
 
 except KeyboardInterrupt:
     ser.close()  # Dong Port noi tiep
